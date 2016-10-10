1ba2edd19e2530544b2bf3aa54a2b2f513604070
corbelld <corbelld@oregonstate.edu>
2016-10-10 03:44:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ config = {'default-group':'base-data'}
 
 application = webapp2.WSGIApplication([
   ('/edit', 'edit.Edit'),
-  #('/view.html', 'base_page.View'),
+  ('/view.html', 'base_page.View'),
   ('/', 'base_page.Dynamic'),
 ], debug=True, config=config)
